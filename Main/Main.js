c43cb6f176295ff90354c093f0fb77f829b71763
yunju <duswldbswn@pusan.ac.kr>
2023-11-28 06:56:16 +0900

    feat : main에 상품 가져오기

--- Main/Main.js
+++ Main/Main.js
@@ -1,3 +1,36 @@
 import { createNavbar } from '../Component/Navbar.js';
       
-document.addEventListener('DOMContentLoaded', createNavbar);
+        document.addEventListener('DOMContentLoaded', createNavbar);
+
+
+async function fetchProducts() {
+    const response = await fetch('http://localhost:3000/products');
+    const products = await response.json();
+  
+   
+    const productList = document.getElementById('product-list');
+  
+    // 각 상품에 대해 카드를 생성
+    products.forEach(product => {
+      const card = document.createElement('div');
+      card.className = 'product-card';
+  
+      const img = document.createElement('img');
+      img.src = product.image;
+      card.appendChild(img);
+  
+      const title = document.createElement('h2');
+      title.textContent = product.title;
+      card.appendChild(title);
+  
+      const price = document.createElement('p');
+      price.textContent = product.price;
+      card.appendChild(price);
+  
+      productList.appendChild(card);
+    });
+  }
+  
+  // 페이지 로드시 상품 정보를 가져옴
+  window.onload = fetchProducts;
+  
